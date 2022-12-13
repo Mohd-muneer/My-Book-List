98d9591d0707eb06d4c003efffe1daa7460e41a5
Mohd-muneer <mohdmuneeruddin456@gmail.com>
2022-12-13 15:14:24 +0530

    UI-Using-Bootstrap

---
+++ app.js
@@ -0,0 +1,11 @@
+// functionality that we are going to perform 
+
+// BOOK class:represents a book
+
+// UI Class: Handles UI Tasks
+
+// STORE Class: Handles Store
+
+// Event: Add a Book
+
+// Event: Remove a Book
