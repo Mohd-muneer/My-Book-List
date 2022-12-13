8e74c0e96fe038859122fe63a8eca761942a3099
Mohd-muneer <mohdmuneeruddin456@gmail.com>
2022-12-13 23:52:51 +0530

    JS_o.1

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 // functionality that we are going to perform 
 
 // BOOK class:represents a book
+class Book {
+    constructor(title,author,isbn) {
+        this.title=title;
+        this.author=author;
+        this.isbn=isbn;
+    }
+}
+
 
 // UI Class: Handles UI Tasks
+class UI {
+    static displayBooks(){
+        
+    }
+}
 
 // STORE Class: Handles Store
 
